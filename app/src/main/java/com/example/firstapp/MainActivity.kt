1bf490a7c206df38bb2485c6a6d59d7f2aea1e22
Nikita Tishchenko <nt3008@yandex.ru>
2024-10-01 18:45:07 +0300

    ADD all

---
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.firstapp
+
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val listView = findViewById<ListView>(R.id.listView)
+        val userData: EditText = findViewById(R.id.user_data)
+        val button: Button = findViewById(R.id.button)
+
+        val todos: MutableList<String> = mutableListOf();
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, todos);
+        listView.adapter = adapter;
+        button.setOnClickListener {
+            val text = userData.text.toString().trim()
+            if(text != "")
+                adapter.insert(text, 0)
+        }
+
+
+    }
+}
